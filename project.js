d0c26edfd016b115f6f9e303349d659769889d6c
shilpa2103 <shilpa.s@appinessworld.com>
2022-05-16 08:29:42 +0530

    first commit

---
+++ project.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+window.onscroll = () =>{
+
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 50)
+  {
+    document.querySelector('#scroll-top').classList.add('active');
+  }
+  else
+  {
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+}
+
+
+
+
+
+
+
